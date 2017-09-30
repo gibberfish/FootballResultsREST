9bff6b33a5b56e066b356a42307250a1e23c11d1
Mark Fowler <themindbadger@gmail.com>
2017-09-30 07:09:54 +0100

    Teams are returned, but only 2...

--- src/main/java/mindbadger/football/api/repository/SeasonDivisionToTeamRepository.java
+++ src/main/java/mindbadger/football/api/repository/SeasonDivisionToTeamRepository.java
@@ -1,35 +1,25 @@
 package mindbadger.football.api.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.RelationshipRepositoryBase;
 import io.crnk.core.resource.list.ResourceList;
-import mindbadger.football.api.model.KatharsisDivision;
 import mindbadger.football.api.model.KatharsisSeason;
 import mindbadger.football.api.model.KatharsisSeasonDivision;
 import mindbadger.football.api.model.KatharsisTeam;
-import mindbadger.football.domain.Season;
-import mindbadger.football.domain.SeasonDivision;
-import mindbadger.football.repository.SeasonRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @Component
 public class SeasonDivisionToTeamRepository extends RelationshipRepositoryBase<KatharsisSeasonDivision, String, KatharsisTeam, String> {
 
     private KatharsisSeasonRepository seasonRepository;
-    private KatharsisTeamRepository teamRepository;
 
     @Autowired
     public SeasonDivisionToTeamRepository(KatharsisSeasonRepository seasonRepository,KatharsisTeamRepository teamRepository) {
         super (KatharsisSeasonDivision.class, KatharsisTeam.class);
         this.seasonRepository = seasonRepository;
-        this.teamRepository = teamRepository;
     }
 
     @Override
@@ -40,19 +30,18 @@ public class SeasonDivisionToTeamRepository extends RelationshipRepositoryBase<K
     @Override
     public ResourceList<KatharsisTeam> findManyTargets(String sourceId, String fieldName, QuerySpec querySpec) {
     	KatharsisSeason season = seasonRepository.findOne(parseSeasonIdFromSourceId(sourceId), querySpec);
-    	Set<KatharsisSeasonDivision> seasonDivisions = season.getSeasonDivisions();
-    	//TODO How do we get a single season division, from which we can get the teams?
+    	
+    	for (KatharsisSeasonDivision seasonDivision : season.getSeasonDivisions() ) {
+    		if (sourceId.equals(seasonDivision.getId())) {
+    			return querySpec.apply(seasonDivision.getTeams());
+    		}
+    	}
     	return null;
     }
 
     private Integer parseSeasonIdFromSourceId (String sourceId) {
     	String[] idSplit = sourceId.split("-");
     	return Integer.parseInt(idSplit[0]);
-    }
-
-    private String parseDivisionIdFromSourceId (String sourceId) {
-    	String[] idSplit = sourceId.split("-");
-    	return idSplit[1];
     }
 
     @Override

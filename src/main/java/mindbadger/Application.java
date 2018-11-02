7309d688de9c8956f7b4267bc79f445240c67c5d
Mr Gibberfish <mrgibberfish@gmail.com>
2018-11-02 10:31:37 +0000

    Removed swagger2 code from app startup

--- src/main/java/mindbadger/Application.java
+++ src/main/java/mindbadger/Application.java
@@ -16,11 +16,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
 //@Configuration
 @RestController
 @SpringBootApplication(scanBasePackages="**/mindbadger/**/*")
@@ -48,14 +43,5 @@ public class Application {
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-    
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
     }
 }
